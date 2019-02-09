b13331a18c64a9a2bcc5f5d50ee2be75d061bc93
Elijah Scheele <eli@zanycadence.com>
2019-02-08 21:43:31 -0800

    initial commit of project for styling hackathon project

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleancss = require('gulp-clean-css');
+var csscomb = require('gulp-csscomb');
+var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var pug = require('gulp-pug');
+
+var paths = {
+    source: './*.scss',
+};
+
+gulp.task('build', function () {
+    gulp.src(paths.source)
+        .pipe(sass({
+                outputStyle: 'compact',
+                precision: 10
+            })
+            .on('error', sass.logError)
+        )
+        .pipe(autoprefixer())
+        .pipe(csscomb())
+        .pipe(gulp.dest('./css'))
+        .pipe(cleancss())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('default', ['build']);
